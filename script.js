54c122eb9552c27523c3b14a065cf718877ecd19
rozayildirim <roza_yildirim@hotmail.com>
2024-01-10 00:35:48 +0300

    Ana dosyalar eklendi

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    var timeString = hours + ':' + minutes + ':' + seconds;
+
+    document.getElementById('clock').textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // initial call to display clock immediately
